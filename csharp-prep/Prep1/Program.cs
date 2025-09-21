425c8ceebe2b758a3ebad8064af754e1e404f01e
Pat Blau <patblau@MacBookPro.ht.home>
2025-09-20 22:21:56 -0500

    fixed errors

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,14 +2,24 @@ using System;
 
 internal class Program
 {
-    private static void Main(string[] args)
-    {
-        Console.Write("What is your name? ");
-        string first = Console.ReadLine();
+	private static void Main(string[] args)
+	{
+		Console.WriteLine("Hello World!");
+		Console.Write("What is your last name? ");
 
-        Console.Write("What is your last name? ");
-        string last = Console.ReadLine();
+		Console.Write("What is your favorite color? ");
+		string color = Console.ReadLine();
+		Console.WriteLine($"Your favorite color is {color}");
 
-        Console.WriteLine($"Your name is {last}, {first} {last}!");
-    }
+		int number = 5;
+		number = 8;
+		number = number + 3;
+
+		string favColor = "blue";
+		if (number > 3)
+		{
+			// Do something if needed
+		}
+	}
 }
+
