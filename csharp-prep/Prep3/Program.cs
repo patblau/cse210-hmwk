eb27337532a1b42f9612a28b46984fb0fe97ee8f
Pat Blau <patblau@MacBookPro.ht.home>
2025-09-29 23:08:33 -0500

    Setting up Prep 3 coding

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,55 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Linq; // for helpful list methods like Min, Max, Average
 
 class Program
 {
-    static void Main()
-    {
-        string playAgain = "yes";
-
-        while (playAgain.ToLower() == "yes")
-        {
-            // Generate a random number between 1 and 100
-            Random randomGenerator = new Random();
-            int magicNumber = randomGenerator.Next(1, 101);
-
-            int guess = -1;
-            int guessCount = 0;
-
-            Console.WriteLine("I'm thinking of a number between 1 and 100...");
-
-            // Loop until the user guesses correctly
-            while (guess != magicNumber)
-            {
-                Console.Write("Can you guess what it is? ");
-                string input = Console.ReadLine();
-
-                // Make sure input is valid
-                if (!int.TryParse(input, out guess))
-                {
-                    Console.WriteLine("Please enter a your number.");
-                    continue;
-                }
-
-                guessCount++;
-
-                if (guess < magicNumber)
-                {
-                    Console.WriteLine("Higher");
-                }
-                else if (guess > magicNumber)
-                {
-                    Console.WriteLine("Lower");
-                }
-                else
-                {
-                    Console.WriteLine($"🎉 You guessed it in {guessCount} tries!");
-                }
-            }
-
-            Console.Write("Do you want to play again (yes/no)? ");
-            playAgain = Console.ReadLine();
-        }
-
-        Console.WriteLine("Thanks for playing! Goodbye 👋");
-    }
-}
+    static void Main(string[] args) 
