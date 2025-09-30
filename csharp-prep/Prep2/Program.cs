50b29d171707d3d9edce87b16d9221efbc9ea899
Pat Blau <patblau@MacBookPro.ht.home>
2025-09-29 22:57:14 -0500

    finished the code

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -19,7 +19,7 @@ class Program
         // Main game loop (play again feature)
         string playAgain = "yes";
         while (playAgain.ToLower() == "yes")
-        { 
+        {
             // Generate random magic number (1–100)
             Random randomGenerator = new Random();
             int magicNumber = randomGenerator.Next(1, 101);
@@ -28,4 +28,35 @@ class Program
             int guessCount = 0;
 
             Console.WriteLine("\nI am thinking of a magic number between 1 and 100...");
-}   
+            
+            // Guessing loop
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {guessCount} guesses.");   
+
+                    // Ask to play again
+                    Console.Write("Would you like to play again? (yes/no) ");
+                    playAgain = Console.ReadLine().ToLower();
+                    if (playAgain != "yes")
+                    
+                    Console.WriteLine("Thanks for playing! Goodbye.");          
+                }
+            }
+        }
+    }
+}
