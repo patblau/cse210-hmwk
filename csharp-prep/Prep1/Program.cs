9de7715ad8ee8d8b79da015cc415bbedaa4d7444
Pat Blau <patblau@MacBookPro.ht.home>
2025-09-29 20:25:13 -0500

    code for second question

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,22 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-		Console.WriteLine("Hello World!");
+	# ask 
+		Console.WriteLine("Hello there!");
 		Console.Write("What is your last name? ");
 		string lastName = Console.ReadLine();
 
-		Console.Write("What is your favorite color? ");
-		string color = Console.ReadLine();
-		Console.WriteLine($"Your favorite color is {color}");
-
-		int number = 5;
-		number = 8;
-		number = number + 3;
-
-		string anotherColor = "blue";
-		if (number > 3)
-		{
-			Console.WriteLine("Number is greater than 3.");
-		}
-	}
-}
+		Console.Write("What is your first name? ");
+		string firstName = Console.ReadLine();
+		
