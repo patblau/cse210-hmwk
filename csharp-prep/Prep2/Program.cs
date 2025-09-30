06e49e8d13aa661be2b49ad3be72efbb1a8d12bb
Pat Blau <patblau@MacBookPro.ht.home>
2025-09-29 22:38:20 -0500

    added Prep 1 code

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
-}
+        Console.WriteLine("Hello there!");
+
+        // Ask for first and last name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Display formatted name
+        Console.WriteLine($"\nYour name is {lastName}, {firstName} {lastName}.");
+    }   
