255a4b08271edf44ec5e58482787ef1de0b599cf
Pat Blau <patblau@MacBookPro.ht.home>
2025-09-29 23:12:06 -0500

    Request number list

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,4 +4,11 @@ using System.Linq; // for helpful list methods like Min, Max, Average
 
 class Program
 {
-    static void Main(string[] args) 
+    static void Main(string[] args)
+    {
+        // Request numbers from the user
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int userNumber = -1; 
