33e045cbf3358b692fb1bf57d20cb45cc0ccf00c
Pat Blau <patblau@MacBookPro.ht.home>
2025-09-29 20:37:13 -0500

    create display for name

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,11 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-	# ask 
 		Console.WriteLine("Hello there!");
+
+		// Ask for first name
 		Console.Write("What is your last name? ");
 		string lastName = Console.ReadLine();
 
-		Console.Write("What is your first name? ");
+		// Ask for last name
+		Console.Write("What is your last name? ");
 		string firstName = Console.ReadLine();
+
+		// Display full name
+		Console.WriteLine($"Your name is, {lastName}!");
+		Console.WriteLine($"{firstName} {lastName}");
+	}	
+}
+
+
 		
