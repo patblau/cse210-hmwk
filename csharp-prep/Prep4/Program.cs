ee0cfd7ac21e199df820b611a08d9fbb0b6cb515
Pat Blau <patblau@MacBookPro.ht.home>
2025-09-29 23:37:41 -0500

    Definitions

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
+
         Console.WriteLine("Hello Prep4 World!");
+        }
+            DisplayWelcome();
+
+            string name = PromptUserName();
+            int favoriteNumber = PromptUserNumber();
+
+            PromptUserBirthYear(out int birthYear);
+
+            int squaredNumber = SquareNumber(favoriteNumber);
+
+            DisplayResult(name, squaredNumber, birthYear);
     }
-}
+}       
