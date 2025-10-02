25017a6252945f27d22d602f72402d0fe0085997
Pat Blau <patblau@MacBookPro.ht.home>
2025-10-02 10:46:34 -0500

    Starting collection list codes

--- prove/Develop03/ScriptRespository.cs
+++ prove/Develop03/ScriptRespository.cs
@@ -2,3 +2,21 @@ using System;
 using System.Collections.Generic;
 
 public class ScriptureRepository
+{
+    // Collection -> list of scriptures
+    private readonly Dictionary<StandardWork, List<ScriptureInfo>> _data
+        = new Dictionary<StandardWork, List<ScriptureInfo>>();
+
+    public ScriptureRepository()
+    {
+        SeedDefaults();
+    }
+
+    /// <summary>
+    /// Get all scriptures in a given collection (never null).
+    /// </summary>
+    public List<ScriptureInfo> GetByStandardWork(StandardWork work)
+    {
+        if (_data.TryGetValue(work, out var list)) return list;
+        return new List<ScriptureInfo>();
+    }
