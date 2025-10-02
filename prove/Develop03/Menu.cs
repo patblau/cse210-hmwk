8eb38ee001f7574f461e331688dbf9789c790675
Pat Blau <patblau@MacBookPro.ht.home>
2025-10-02 11:10:15 -0500

    Setting up private and public behaviors

--- prove/Develop03/Menu.cs
+++ prove/Develop03/Menu.cs
@@ -3,3 +3,9 @@ using System.Collections.Generic;
 
 public class Menu
 {
+    private readonly ScriptureRepository _repo;
+
+    public Menu(ScriptureRepository repo)
+    {
+        _repo = repo;
+    }
