2ae348c1eb5c4ecac88b74e8f64e48b147c0bbed
Pat Blau <patblau@MacBookPro.ht.home>
2025-10-01 22:54:33 -0500

    Did code for Scriptures

---
+++ prove/Develop03/ScripInfo.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private readonly Reference _reference;
+    private readonly List<Word> _words = new List<Word>();
+    private readonly Random _random = new Random();
+    private readonly string _originalText;
+
+    public int TotalCount => _words.Count;
+    public int HiddenCount { get { int c=0; foreach (var w in _words) if (w.IsHidden()) c++; return c; } }
+    public double HiddenPercent => _words.Count == 0 ? 0 : HiddenCount * 100.0 / _words.Count;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _originalText = text;
+        var parts = text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        foreach (var p in parts) _words.Add(new Word(p));
+    }
+
+    public void DisplayInline()
+    {
+        foreach (var w in _words) { Console.Write(w); Console.Write(' '); }
+    }
+
+    public void HideRandomWords(int count)
+    {
+        var visible = new List<int>();
+        for (int i=0; i<_words.Count; i++) if (!_words[i].IsHidden()) visible.Add(i);
+        for (int i=0; i<count && visible.Count>0; i++)
+        {
+            int pick = _random.Next(visible.Count);
+            _words[visible[pick]].Hide();
+            visible.RemoveAt(pick);
+        }
+    }
+
+    public void RevealOneRandomHidden()
+    {
+        var hidden = new List<int>();
+        for (int i=0; i<_words.Count; i++) if (_words[i].IsHidden()) hidden.Add(i);
+        if (hidden.Count == 0) return;
+        _words[hidden[_random.Next(hidden.Count)]].Reveal();
+    }
+
+    public void Reset()
+    {
+        _words.Clear();
+        var parts = _originalText.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        foreach (var p in parts) _words.Add(new Word(p));
+    }
+
+    public bool AllHidden()
+    {
+        foreach (var w in _words) if (!w.IsHidden()) return false;
+        return true;
+    }
+}
