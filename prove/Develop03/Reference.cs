92b1da39e49282275a0ed4455977b9be1437a0ab
Pat Blau <patblau@MacBookPro.ht.home>
2025-10-01 22:39:04 -0500

    removed Develop03 access code

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -1,36 +1,24 @@
-namespace Develop03
+public class Reference
 {
-    /// <summary>Stores a scripture reference (single verse or range).</summary>
-    public class Reference
+    public string Book { get; }
+    public int Chapter { get; }
+    public int StartVerse { get; }
+    public int EndVerse { get; }
+
+    public Reference(string book, int chapter, int verse)
     {
-        public string Book { get; }
-        public int Chapter { get; }
-        public int StartVerse { get; }
-        public int EndVerse { get; }
+        Book = book; Chapter = chapter; StartVerse = verse; EndVerse = verse;
+    }
 
-        // Single verse
-        public Reference(string book, int chapter, int verse)
-        {
-            Book = book;
-            Chapter = chapter;
-            StartVerse = verse;
-            EndVerse = verse;
-        }
+    public Reference(string book, int chapter, int startVerse, int endVerse)
+    {
+        Book = book; Chapter = chapter; StartVerse = startVerse; EndVerse = endVerse;
+    }
 
-        // Verse range
-        public Reference(string book, int chapter, int startVerse, int endVerse)
-        {
-            Book = book;
-            Chapter = chapter;
-            StartVerse = startVerse;
-            EndVerse = endVerse;
-        }
-
-        public override string ToString()
-        {
-            return StartVerse == EndVerse
-                ? $"{Book} {Chapter}:{StartVerse}"
-                : $"{Book} {Chapter}:{StartVerse}-{EndVerse}";
-        }
+    public override string ToString()
+    {
+        return StartVerse == EndVerse
+            ? $"{Book} {Chapter}:{StartVerse}"
+            : $"{Book} {Chapter}:{StartVerse}-{EndVerse}";
     }
 }
