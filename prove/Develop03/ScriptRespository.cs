028a0cd16b9face80c9cb74d63679526b5c7bb13
Pat Blau <patblau@MacBookPro.ht.home>
2025-10-02 10:55:54 -0500

    Added New Testament

--- prove/Develop03/ScriptRespository.cs
+++ prove/Develop03/ScriptRespository.cs
@@ -52,3 +52,15 @@ public class ScriptureRepository
                 "In all thy ways acknowledge him, and he shall direct thy paths."
             )
         );
+
+        // ============================
+        // New Testament
+        // ============================
+        Add(StandardWork.NewTestament,
+            new ScriptureInfo(
+                new Reference("John", 3, 16),
+                "For God so loved the world, that he gave his only begotten Son, " +
+                "that whosoever believeth in him should not perish, but have everlasting life."
+            )
+        );
+
