3a592314b0f226deab19e3dc2d07fe86fd058b6d
Pat Blau <patblau@MacBookPro.ht.home>
2025-10-07 23:59:58 -0500

    added test

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -25,5 +25,14 @@ class Program
         {
             return $"{_studentName} - {_topic}";
         }
+
+        // test method
+        public static void Test()
+        {
+            Assignment a1 = new Assignment("Pat Blau", "Programming with Classes");
+            Console.WriteLine(a1.GetSummary());
+        }
     }
+}
+
        
