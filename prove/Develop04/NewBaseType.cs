d6f44e5346e0e37c50ab038a9e5f1d89500dfdbc
Pat Blau <patblau@MacBookPro.ht.home>
2025-10-08 01:11:40 -0500

    changed to NewbaseType

---
+++ prove/Develop04/NewBaseType.cs
@@ -0,0 +1,13 @@
+
+using System;
+
+internal class NewBaseType
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello Develop04 World!");
+
+        //Test it
+        Assignment a1 = new Assignment("Pat Blau", "Encapsulation");
+    }
+}
