d633d21a04b9349da01894f85a5f48273a146fd4
Pat Blau <patblau@MacBookPro.ht.home>
2025-10-18 00:20:10 -0500

    Added hour, minute, and message code

--- prove/Develop05/Reminder.cs
+++ prove/Develop05/Reminder.cs
@@ -9,6 +9,13 @@ namespace Prove.Develop05
         private bool _isEnabled;
         private string _message;
 
-       
+        public Reminder(int hour, int minute, string message = "")
+        {
+            _reminderTime = new TimeSpan(hour, minute, 0);
+            _isEnabled = true;
+        }
+
+       ();
+        }
     }
 }
