4dfad9c7c632baa9b37f8f3f9a8c844bd982523d
Pat Blau <patblau@MacBookPro.ht.home>
2025-10-18 00:18:50 -0500

    Starting Reminder class codes

---
+++ prove/Develop05/Reminder.cs
@@ -0,0 +1,14 @@
+
+using System;
+using System.Runtime.CompilerServices;
+namespace Prove.Develop05
+{
+    public class Reminder
+    {
+        private TimeSpan _reminderTime;
+        private bool _isEnabled;
+        private string _message;
+
+       
+    }
+}
