f2de03031dce7dbec87d5e6f947952c6933281a1
pat blau <patblau2330@gmail.com>
2025-10-23 14:39:45 -0500

    fixed format and added list for customers only

--- video-abstraction/Video.cs
+++ video-abstraction/Video.cs
@@ -4,6 +4,7 @@ using System.Dynamic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Transactions;
+using System.Xml;
 
 namespace BFAdventureVideos
 {
@@ -13,18 +14,22 @@ namespace BFAdventureVideos
         public string Title { get; }
         public string Aughtor { get; }
         public int VideoLength { get; }
-        
-            //Comment section//
-            //Internal storage//
+
+        //Comment section//
+        //Internal storage//
         private readonly List<Commemt> _comments = new();
         private readonly List<string> _keywords = new();
-        }
-            //Customer list (readonly)//
+    
+        //List for customers (readonly)//
+        public IReadOnlyList<Comment> Comments => _comment;
+        public IReadOnly List<string> Keywords => _keyworks;
 
-                //Strings for title, author, and videoLength
-                //Add command
-                //Add keyword
-            
+        //Strings for title, author, and videoLength
+        //Add command
+        //Add keyword
+    { 
+{ 
+
         
        
 
