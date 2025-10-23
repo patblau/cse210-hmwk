f9c8621abdbd4a4c0e64d70b19d76d68323b25d6
pat blau <patblau2330@gmail.com>
2025-10-23 14:05:13 -0500

    set up base class

---
+++ video-abstraction/Video.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Transactions;
+
+namespace BFAdventureVideos
+{
+    //Bass class; title, author, length, comments and Keyworkd//
+    public class Video
+    {
+        public string Title { get; }
+        public string Aughtor { get; }
+        public int LengthSeconds { get; }
+
+       
+    
+
+        
+    
+
+
