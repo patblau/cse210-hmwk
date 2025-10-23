1a1e3a916967c7cfb7b8c897783669715b3c3707
pat blau <patblau2330@gmail.com>
2025-10-23 00:11:26 -0500

    Started class Commet

---
+++ video-abstraction/Comment.cs
@@ -0,0 +1,10 @@
+using System;
+using System.ComponentModel;
+using System.Dynamic;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+namespace BFAdventureVideos
+{
+    /// <summary>Value object for a video comment.</summary>
+    public class Comment
+    {
