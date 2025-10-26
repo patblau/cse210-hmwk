46d5931280e40b3ad2386391399141cd72a78d86
pat blau <patblau2330@gmail.com>
2025-10-26 04:28:58 -0500

    starting GuidedHike,cs

---
+++ final/Foundation4/Guided Hike.cs
@@ -0,0 +1,9 @@
+using System;
+
+// GuidedHike.cs
+// Derived: difficulty + miles + guest impact
+
+namespace BFACampingOps
+{
+    //Enum for trail difficulty
+    public enum HikeDifficulty { Easy, Moderate, Hard }
