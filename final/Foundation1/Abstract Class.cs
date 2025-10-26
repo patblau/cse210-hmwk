091da8b10944a561b04c9775d7510c57d8780866
pat blau <patblau2330@gmail.com>
2025-10-25 22:08:34 -0500

    added public readonly codes

--- final/Foundation1/Abstract Class.cs
+++ final/Foundation1/Abstract Class.cs
@@ -13,8 +13,14 @@ namespace BFAdventureVideos
     // Private Fields (Hidden Data) to stor coment and keywords
         
     private readonly List<Comment> _comments = new();
-    private readonly List<string> _keywords = new();
+        private readonly List<string> _keywords = new();
 
+    //Public Properties (Part of the Interface), ReadOnly title, author, VideoLength)
+    public string Title { get; }
+    public string Author { get; }
+    public int VideoLength { get; }
+    public IReadOnlyList<Comment> Comments => _comments;
+    public IReadOnlyList<string> Keywords => _keywords;
     
     }
 
