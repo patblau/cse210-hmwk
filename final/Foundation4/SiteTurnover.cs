08f39c4d4ef8f175011d1d61a8f37ac45a6d13bb
pat blau <patblau2330@gmail.com>
2025-10-26 03:58:33 -0500

    set up mutators

--- final/Foundation4/SiteTurnover.cs
+++ final/Foundation4/SiteTurnover.cs
@@ -9,8 +9,22 @@ namespace BFACampingOps
     public class SiteTurnover : CampActivity
     {
         // 1) Specific flags
-        public bool Cleaned   { get; private set; }
+        public bool Cleaned { get; private set; }
         public bool Inspected { get; private set; }
-        public bool Stocked   { get; private set; }
-        public bool OnTime    { get; private set; }
-   }
+        public bool Stocked { get; private set; }
+        public bool OnTime { get; private set; }
+    }
+   
+   // Constructor
+        public SiteTurnover(string notes = "", int minutes = 0, int staff = 0, int guests = 0,
+                            bool cleaned = false, bool inspected = false, bool stocked = false, bool onTime = false)
+            : base("Site Turnover", notes, minutes, staff, guests)
+        {
+            Cleaned = cleaned; Inspected = inspected; Stocked = stocked; OnTime = onTime;
+        }
+
+        // 3) Mutators
+        public void MarkCleaned(bool v = true)  => Cleaned = v;
+        public void MarkInspected(bool v = true)=> Inspected = v;
+        public void MarkStocked(bool v = true)  => Stocked = v;
+        public void MarkOnTime(bool v = true)   => OnTime = v;
