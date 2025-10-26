dcfeecf657dc48a12f6a62c426ed64f977908f6a
pat blau <patblau2330@gmail.com>
2025-10-25 22:59:07 -0500

    creating program class

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,4 +1,12 @@
 using System;
+using System.Collections.Generic;
+using BFAdventureVideos;   // Gives access to your custom classes
+
+// Program.cs
+// Entry point for BFAdventureVideos project
+// Demonstrates Abstraction
+
+
 
 class Program
 {
