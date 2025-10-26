a1936a4ac5726287c36052e07b5165b47bff5dbe
pat blau <patblau2330@gmail.com>
2025-10-26 04:48:55 -0500

    Starting BFACampingOps class

---
+++ final/Foundation4/Riverboat.cs
@@ -0,0 +1,9 @@
+using System;
+
+// RiverFloat.cs
+// Derived: river class + distance + boats
+
+namespace BFACampingOps
+{
+    // 0) Simple whitewater class enum (I–III)
+    public enum RiverClass { I, II, III }
