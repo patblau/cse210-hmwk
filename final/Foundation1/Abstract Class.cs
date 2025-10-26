97139cf2d8cda78f5de2d2a907363b4630f76321
pat blau <patblau2330@gmail.com>
2025-10-25 21:57:53 -0500

    coded FacilityVideo class

---
+++ final/Foundation1/Abstract Class.cs
@@ -0,0 +1,14 @@
+using System;
+
+// FacilityVideo.cs
+// Specialized class for facility-related videos.
+// Uses Abstraction with focued property usage
+// by using the base Video functions.
+
+
+namespace BFAdventureVideos
+{
+    public class FacilityVideo : ThemedVideo
+    {
+    }
+}
