a9bbb6e68a80f5625ad1a31f7896f0546c4ac59f
pat blau <patblau2330@gmail.com>
2025-10-25 22:53:38 -0500

    created ActivityVideo class

---
+++ final/Foundation1/ActvityVideo.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace BFAdventureVideo
+{
+    // ActivityVideo.cs
+    // Specialized class for videos about activities and events.
+    // Demonstrates Abstraction:
+
+
+}public class ActivityVideo : ThemedVideo
+    {
+        // Public Property – simple, meaningful interface addition
+        public string ActivityType { get; }
+
+       
+}
