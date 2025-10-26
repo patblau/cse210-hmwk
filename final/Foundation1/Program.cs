f2a6a9cd981dadf780e21d9450e7a22905bab8c8
pat blau <patblau2330@gmail.com>
2025-10-25 23:08:07 -0500

    added vidoe var v1 list codes

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -10,5 +10,19 @@ class Program
 {
     static void Main()
     {
-    }
-}
+        // Create a List to hold all video objects.
+       
+        List<Video> videos = new List<Video>();
+
+        // Create different video types.
+        // Each class shares the same public interface from Video, while having its own specialized data.
+       
+        var v1 = new FacilityVideo(
+            "Facility Tour: Lodges & Outdoor Kitchens",
+            "BFAdventure",
+            210,
+            "Main Lodge and Outdoor Kitchen");
+        v1.AddComment(new Comment("Jamie", "Loved the pavilions!"));
+        v1.AddComment(new Comment("Chris", "Facilities look so clean!"));
+
+        
