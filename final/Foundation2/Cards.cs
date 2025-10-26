b4f214951d47e3f483e5d8fa7c5473f6bb0a60fe
pat blau <patblau2330@gmail.com>
2025-10-25 23:46:56 -0500

    added public read codea

--- final/Foundation2/Cards.cs
+++ final/Foundation2/Cards.cs
@@ -9,9 +9,18 @@ public class Card
 {
     // Private backing fields (hidden)
     private string _player;
-    private int    _year;
+    private int _year;
     private string _brand;
     private string _condition;
     private decimal _valueUsd;
+
+    
+    // Public read-only properties (expose safely)
+    public string Player     => _player;
+    public int    Year       => _year;
+    public string Brand      => _brand;
+    public string Condition  => _condition;
+    public decimal ValueUsd => _valueUsd;
+    
 }
 
