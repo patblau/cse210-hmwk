98a0a21a9efd6de7a055afba0593f280d4425cf7
pat blau <patblau2330@gmail.com>
2025-10-25 22:45:24 -0500

    created CampingSite class

---
+++ final/Foundation1/CampingSite.cs
@@ -0,0 +1,14 @@
+using System;
+
+// CampingSiteVideo.cs
+// Specialized class for camping sites videos.
+// Demonstrate Abstractionand add a new property while reusing base class features.
+
+namespace BFAdventureVideos
+{
+    //videos for camping sites: RV pads, tents, yurts, etc.
+    // Shows a subclass extending the base without revealing its internals processes.
+    public class CampingSiteVideo : ThemedVideo
+    {
+    }
+}
