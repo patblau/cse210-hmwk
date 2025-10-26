cbb2ec07df13a077d72be785ac4f1a1b23789104
pat blau <patblau2330@gmail.com>
2025-10-26 04:31:35 -0500

    added conductors

--- final/Foundation4/Guided Hike.cs
+++ final/Foundation4/Guided Hike.cs
@@ -7,3 +7,10 @@ namespace BFACampingOps
 {
     //Enum for trail difficulty
     public enum HikeDifficulty { Easy, Moderate, Hard }
+
+    public class GuidedHike : CampActivity
+    {
+        // Specific fields
+        public HikeDifficulty Difficulty { get; private set; }
+        public double Miles { get; private set; }
+        
