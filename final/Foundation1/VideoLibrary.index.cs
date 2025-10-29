7e110cf85cf0a1b980406381016938e2fa9a800f
pat blau <patblau2330@gmail.com>
2025-10-28 23:10:46 -0500

    added public codes.

--- final/Foundation1/VideoLibrary.index.cs
+++ final/Foundation1/VideoLibrary.index.cs
@@ -3,3 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 
 public class VideoLibrary
+{
+    public class VideoLibrary
+    {
+        private readonly List<Video> _videos = new();
+
+        // Add a single video
+        public void Add(Video v)
+        {
+            if (v != null) _videos.Add(v);
+        }
+    }
+}
