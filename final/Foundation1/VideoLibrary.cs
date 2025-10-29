5df37d48ad2d2e0635981c93128afe2c16dd7ae2
pat blau <patblau2330@gmail.com>
2025-10-28 22:32:16 -0500

    added public codes

--- final/Foundation1/VideoLibrary.cs
+++ final/Foundation1/VideoLibrary.cs
@@ -4,3 +4,10 @@ using System.Linq;
 
 public class VideoLibrary
 {
+     private readonly List<Video> _videos = new();
+
+    // Add a video to the library
+    public void Add(Video v)
+    {
+        if (v != null) _videos.Add(v);
+    }
